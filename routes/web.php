595325dba7fc5faf3a09b213327698d65fd812a6
Kostiantyn Rakilov <rakilovkg@gmail.com>
2024-03-31 22:56:49 +0300

    Added Password model and CRUD routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PasswordController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('passwords', PasswordController::class);
